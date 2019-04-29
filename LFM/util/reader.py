9d1ea74d5299fd2e70211503bd59cd1b3c9bbfee
FlyGreyWolf <504574519@qq.com>
2019-04-29 17:24:22 +0800

    lfm update get_train_data() funciton

--- LFM/util/reader.py
+++ LFM/util/reader.py
@@ -4,7 +4,7 @@ author:xujian
 date:2019****
 """
 import os
-
+import operator
 
 def get_ave_score(input_file):
     if not os.path.exists(input_file):
@@ -45,7 +45,6 @@ def get_movie_info(movie_info_file):
                 continue
 
             movie_info = line.strip().split(',')
-
             if (len(movie_info) < 3):
                 continue
             movie_id, genres = movie_info[0], movie_info[-1]    #-1 means the last value of the array
@@ -56,6 +55,50 @@ def get_movie_info(movie_info_file):
             if movie_id not in movie_info_map:
                 movie_info_map[movie_id] = [movie_name, genres]
     return movie_info_map
+def get_train_data(input_file):
+    if not os.path.exists(input_file):
+        return []
+    score_dict = get_ave_score(input_file)
+    neg_dict = {}
+    pos_dict = {}
+    train_data = []
+    linenum = 0
+    score_thr = 4.0
+    fp = open(input_file)
+    for line in fp:
+        if linenum == 0:
+            linenum += 1
+            continue
+        item = line.strip().split(',')
+        if len(item) < 4:
+            continue
+        userid, itemid, rating = item[0], item[1], float(item[2])
+        if userid not in pos_dict:
+            pos_dict[userid] = []
+        if userid not in neg_dict:
+            neg_dict[userid] = []
+        if rating >= score_thr:
+            pos_dict[userid].append((itemid, 1))
+        else:
+            score = score_dict.get(itemid, 0)
+            neg_dict[userid].append((itemid, score))
+    fp.close()
+    for userid in pos_dict:
+        data_num = min(len(pos_dict[userid]), len(neg_dict.get(userid, [])))
+        if data_num > 0:
+            train_data += [(userid, zuhe[0], zuhe[1]) for zuhe in pos_dict[userid]][:data_num]
+        else:
+            continue
+        #负采样，平均分由高到低进行排序，是为了采到那些平均分高的，但是该用户打分低的，更能反映出该用户不喜欢该物品
+        sorted_neg_list = sorted(neg_dict[userid], key= operator.itemgetter(1), reverse=True)[:data_num]
+        train_data += [(userid, zuhe[0], 0) for zuhe in sorted_neg_list]
+        # debug ============== userid = '1'
+        # if userid == '1':
+        #     print(len(pos_dict[userid]))
+        #     print(len(neg_dict[userid]))
+        #     print(sorted_neg_list)
+        # debug ================
+    return train_data
 
 if __name__ == "__main__":
     # item_info = get_movie_info("../data/movies.txt")
@@ -63,11 +106,12 @@ if __name__ == "__main__":
     # print(len(item_info))
     # print(item_info['1'])
     # print(item_info['11'])
-
-    score_dict = get_ave_score('../data/ratings.txt')
-    print(len(score_dict))
-    print(score_dict['31'])
-
+    # score_dict = get_ave_score('../data/ratings.txt')
+    train_data = get_train_data('../data/ratings.txt')
+    print(len(train_data))
+    print(train_data[:20])
+    # print(len(score_dict))
+    # print(score_dict['31'])
 
 
 
